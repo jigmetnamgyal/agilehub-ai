a482956aeb8c5c63cc95480ac3a477cca3835ec6
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-12-03 03:14:38 +0400

    feat(): height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex h-auto flex-col items-center">
         <NavBar />
-        <div className="h-[400px] w-full flex flex-col justify-center items-center text-center gap-10">
+        <div className="h-[500px] w-full flex flex-col justify-center items-center text-center gap-10">
           <p className="w-[60%] leading-[65px] text-5xl font-extrabold">The <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-red-300 to-yellow-300">Ultimate Empowerment</span> Partner for Your Team.</p>
           <p className="w-[60%] text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla iusto numquam assumenda provident. Officiis esse saepe cumque numquam tempora odio illo dolorum error inventore alias! Aspernatur ducimus exercitationem omnis tempora.</p>
 
