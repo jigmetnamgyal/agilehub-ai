39b9dfa969633d39a3b134d90b91347c492ca722
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-12-03 03:00:49 +0400

    feat(): updated margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,20 @@ export default function Home() {
           <a className="btn bg-yellow-300 text-black rounded-[5px] px-10 hover:text-white hover:bg-transparent hover:border-2 hover:border-yellow-300">Start Using Jaggle</a>
         </div>
         
-        <div className="mockup-browser border border-base-300 w-[80%] h-[600px] z-10">
-        <span className="indicator-item badge bg-yellow-300 p-4 text-black absolute right-0 top-3 -rotate-12">Work 10x faster</span> 
-          <div className="mockup-browser-toolbar">
-            <div className="input border border-base-300">https://www.jaggleai.com</div>
+        <div className="mockup-browser border border-base-300 w-[80%] h-[600px] mt-14">
+          <span className="indicator-item badge bg-yellow-300 p-4 text-black absolute right-0 top-3 -rotate-12">Work 10x faster</span> 
+            <div className="mockup-browser-toolbar">
+              <div className="input border border-base-300">https://www.jaggleai.com</div>
+            </div>
+            <div className="flex justify-center px-4 py-16 border-t border-base-300"></div>
+      </div>
+
+      <div className="hero h-auto relative p-52">
+        <div className="hero-content text-center">
+          <div className="w-[70%]">
+            <h1 className="text-5xl leading-[65px] font-bold">Jaggle your coworker, supercharged by AI.</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
-          <div className="flex justify-center px-4 py-16 border-t border-base-300">
         </div>
       </div>
 
