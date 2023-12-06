2c4d0e30e9ebc1cec54490c74c92374dec511189
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-12-07 03:45:02 +0400

    clean up

--- src/app/editor/[editor_id]/page.tsx
+++ src/app/editor/[editor_id]/page.tsx
@@ -2,11 +2,9 @@
 
 import { BlockNoteEditor } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import { useParams } from "next/navigation";
 import "@blocknote/core/style.css";
 
 const Editor = () => {
-  const params = useParams();
   const editor: BlockNoteEditor = useBlockNote({});
 
   return (
