91877d340b4434c90838e57a0ebaf8277f77aea8
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-12-07 03:43:39 +0400

    feat(): blocknote

--- src/app/editor/[editor_id]/page.tsx
+++ src/app/editor/[editor_id]/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { BlockNoteEditor } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import { useParams } from "next/navigation";
+import "@blocknote/core/style.css";
 
 const Editor = () => {
-	const params = useParams();
-	return <div>This is editor page with id {params?.editor_id}</div>;
+  const params = useParams();
+  const editor: BlockNoteEditor = useBlockNote({});
+
+  return (
+    <div>
+      <BlockNoteView editor={editor} />;
+    </div>
+  );
 };
 
 export default Editor;
