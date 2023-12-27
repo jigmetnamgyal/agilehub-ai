ed1b66b24d71e5283df2bd06334acb60cf887952
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-12-28 02:12:04 +0400

    feat(): updated title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,12 @@ export default function Home() {
     <main className="flex h-auto flex-col items-center">
       <NavBar />
 
-      <div className="h-[500px] w-full flex flex-col justify-center items-center text-center gap-10">
-        <p className="w-[60%] leading-[65px] text-5xl font-extrabold">
-          The{" "}
+      <div className="h-[500px] w-full flex flex-col justify-center items-center text-center gap-8">
+        <p className="w-[60%] leading-[50px] text-5xl font-extrabold">
+          When AI meets <br />
           <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-red-300 to-yellow-300">
-            Ultimate Empowerment
+            agile development
           </span>{" "}
-          Partner for Your Team.
         </p>
         <p className="w-[60%] text-sm">
           Elevate your software development game with Jaggle AI. Our AI-driven
